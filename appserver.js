af92cd9e2854c2d2eed847d356e0a6077904b50e
chiloc121100 <109579264+chiloc121100@users.noreply.github.com>
2022-09-21 11:47:32 +0700

    Add files via upload

--- appserver.js
+++ appserver.js
@@ -4,6 +4,18 @@ const appServer = express();
 const router = express.Router();
 const fs = require("fs");
 
+// ------------------- Middleware - kiem soat tinh huong
+router.use( (yeucau, trave, ketiep) => { 
+    console.log("REQ: ", Date.now(), yeucau.url);
+    ketiep();
+});
+
+router.use( (loixayra, yeucau, trave, ketiep) => { 
+    console.log("ERROR: ", Date.now(), yeucau.url);
+    console.log(loixayra);
+    trave.status(500).send("Dang co loi xay ra, chua biet o dau !!!");
+});
+
 // ------------------- Routing
 router.get( "/" , (yeucau, trave) => {
     data = fs.readFileSync("./html/main.html");
@@ -13,6 +25,12 @@ router.get( "/" , (yeucau, trave) => {
 
 router.get( "/home" , (yeucau, trave) => {
     data = fs.readFileSync("./html/home.html");
+    pageContent = data.toString();
+    trave.send(pageContent);
+});
+
+router.get( "/login" , (yeucau, trave) => {
+    data = fs.readFileSync("./html/login.html");
     pageContent = data.toString();
     trave.send(pageContent);
 });
